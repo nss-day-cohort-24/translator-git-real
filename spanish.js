2a679d4946680964ea1c1b7464aa6469627aa653
marcdledda <marcdl636@gmail.com>
2018-02-05 15:34:10 -0600

    spanish.js

---
+++ spanish.js
@@ -0,0 +1,10 @@
+"use strict";
+console.log("spanish.js TEST");
+
+function spanishTranslator(){
+        let toSpanish = "Eres el mejor bolsillo.";
+        return toSpanish;
+        console.log(toSpanish);
+}
+
+module.exports = spanishTranslator;
