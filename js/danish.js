bab7bc346f0c7326cd8f90fd6e83c585de90dd67
Jesie Oldenburg <kitglo46@gmail.com>
2018-02-06 11:23:57 -0600

    changed language to danish; updated methods

---
+++ js/danish.js
@@ -0,0 +1,10 @@
+let danish = document.getElementById('danish');
+danish.addEventListener('click', danishString);
+
+function toDanish(event) {
+    return 'du er den hotteste lomme';
+}
+
+var danishString = toDanish();
+
+module.exports = danishString;
