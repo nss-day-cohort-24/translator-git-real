c02fe8cd8821f79ad1113d1a9a60f7b04642ccc5
Alexis Hennecy <alexishennecy@users-MacBook-Pro.local>
2018-02-05 20:10:53 -0600

    updates to code in german.js

---
+++ js/german.js
@@ -0,0 +1,29 @@
+"use strict";
+
+function german() {
+    const germanText = "Du bist die heißeste Tasche";
+    return germanText
+}
+
+console.log(germanText);
+module.exports = german;
+
+
+
+// let germanElement = document.getElementById("german");
+// germanElement.addEventListener("click", germanText );
+
+
+// create html page that accepts user input, capture user value(getelementbyid), seperate those words out with split, put those words into an array,
+//let array = [];
+//wordarray.foreach(word) let 
+//for each of these words I want you to do something with it, now let me tell you what I want to do with it... let a variable hold info. 
+// .join to put the words in the array back together to be put on the dom
+
+//put the german words in an object and compare the english words entered to the german words in that object
+
+
+//can use .map
+
+//look up if in loop 
+
